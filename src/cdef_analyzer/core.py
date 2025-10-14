48f6121816656fafa1aabdd7d1c4ab9f7630a12e
Larry <lfulton159@gmail.com>
2025-10-14 15:35:33 -0400

    Update core.py

--- src/cdef_analyzer/core.py
+++ src/cdef_analyzer/core.py
@@ -1,8 +1,5 @@
 # src/cdef_analyzer/core.py
-from .gumbel_copula_fixed import (
-    RankDependencyAnalyzer as RankDependencyAnalyzer,
-    CopulaResults as CopulaResults,
-)
+from .gumbel_copula_fixed import RankDependencyAnalyzer, CopulaResults
 
 __all__ = ["RankDependencyAnalyzer", "CopulaResults"]
 
