df1b9901b53cff3a7f08347342a9431cea6c4a2a
Larry <lfulton159@gmail.com>
2025-10-15 08:34:26 -0400

    Update __init__.py

--- src/cdef_analyzer/__init__.py
+++ src/cdef_analyzer/__init__.py
@@ -1,4 +1,4 @@
-# src/cdef_analyzer/__init__.py
 from .gumbel_copula_fixed import RankDependencyAnalyzer, CopulaResults
+from .demo import run_demo_scenarios
 
-__all__ = ["RankDependencyAnalyzer", "CopulaResults"]
+__all__ = ["RankDependencyAnalyzer", "CopulaResults", "run_demo_scenarios"]
